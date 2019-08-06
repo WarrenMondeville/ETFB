e80a0da4365320834a57391340a9a020756296ea
Mondeville <541008505@qq.com>
2019-08-06 18:38:31 +0800

    add heartbeat

--- Unity/Assets/Hotfix/Module/Demo/Helper/LoginHelper.cs
+++ Unity/Assets/Hotfix/Module/Demo/Helper/LoginHelper.cs
@@ -11,20 +11,21 @@ namespace ETHotfix
             {
                 // 创建一个ETModel层的Session
                 ETModel.Session session = ETModel.Game.Scene.GetComponent<NetOuterComponent>().Create(GlobalConfigComponent.Instance.GlobalProto.Address);
-				
+
                 // 创建一个ETHotfix层的Session, ETHotfix的Session会通过ETModel层的Session发送消息
                 Session realmSession = ComponentFactory.Create<Session, ETModel.Session>(session);
-                R2C_Login r2CLogin = (R2C_Login) await realmSession.Call(new C2R_Login() { Account = account, Password = "111111" });
+                R2C_Login r2CLogin = (R2C_Login)await realmSession.Call(new C2R_Login() { Account = account, Password = "111111" });
                 realmSession.Dispose();
 
                 // 创建一个ETModel层的Session,并且保存到ETModel.SessionComponent中
                 ETModel.Session gateSession = ETModel.Game.Scene.GetComponent<NetOuterComponent>().Create(r2CLogin.Address);
                 ETModel.Game.Scene.AddComponent<ETModel.SessionComponent>().Session = gateSession;
-				
+
                 // 创建一个ETHotfix层的Session, 并且保存到ETHotfix.SessionComponent中
                 Game.Scene.AddComponent<SessionComponent>().Session = ComponentFactory.Create<Session, ETModel.Session>(gateSession);
-				
+
                 G2C_LoginGate g2CLoginGate = (G2C_LoginGate)await SessionComponent.Instance.Session.Call(new C2G_LoginGate() { Key = r2CLogin.Key });
+                gateSession.AddComponent<HeartbeatComponent>();
 
                 Log.Info("登陆gate成功!");
 
@@ -36,12 +37,12 @@ namespace ETHotfix
                 Game.EventSystem.Run(EventIdType.LoginFinish);
 
                 // 测试消息有成员是class类型
-                G2C_PlayerInfo g2CPlayerInfo = (G2C_PlayerInfo) await SessionComponent.Instance.Session.Call(new C2G_PlayerInfo());
+                G2C_PlayerInfo g2CPlayerInfo = (G2C_PlayerInfo)await SessionComponent.Instance.Session.Call(new C2G_PlayerInfo());
             }
             catch (Exception e)
             {
                 Log.Error(e);
             }
-        } 
+        }
     }
 }
